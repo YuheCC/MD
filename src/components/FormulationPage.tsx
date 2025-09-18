772b863e2b02fe4652ac05b5ef4ed02a10ee6655
您的GitHub用户名 <@example.com>
2025-09-18 15:10:08 +0800

    Add close button to loading page to return to configuration list

--- src/components/FormulationPage.tsx
+++ src/components/FormulationPage.tsx
@@ -111,7 +111,7 @@ export function FormulationPage({ onNavigateToResults }: FormulationPageProps) {
                 </div>
 
                 {/* Information Box */}
-                <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
+                <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4 mb-6">
                   <div className="flex items-start justify-center">
                     <svg className="w-5 h-5 text-yellow-600 mt-0.5 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
@@ -122,6 +122,15 @@ export function FormulationPage({ onNavigateToResults }: FormulationPageProps) {
                     </div>
                   </div>
                 </div>
+
+                {/* Close Button */}
+                <Button 
+                  variant="outline" 
+                  onClick={() => setCurrentView('list')}
+                  className="px-6 py-2"
+                >
+                  关闭
+                </Button>
               </div>
             </div>
           </div>
